bff923056804f0d22e5f202296044b43e72f11a7
Jimmy Zebroski <zebrozkii@gmail.com>
2019-01-18 11:51:45 -0800

    have finished a total of 4 of the five questions used for the survey working on setting up finale if statements

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,32 @@ $(document).ready(function(){
 var theName = $("input#name").val();
 var theEnviroment = $("#enviroment").val();
 var interest1 =$("#interest1").val();
+var interest2=$("#interest2").val();
 var firstChoice;
+var secondChoice;
 if(theName){
   if(theEnviroment==="Large Company or Business Software"||theEnviroment==="Enterprise-level Company or High Performance Applications"){
     if(interest1==="C#"){
       firstChoice=interest1;
       }else{
         firstChoice=interest1;
+        alert(firstChoice);
       }
-    }else{
-    if (theEnviroment==="Fast Paced and possible government work"||theEnviroment==="Start ups and Smaller companies") {
-      }else {
+  }else{
+    if(interest2==="Ruby"){
+      secondChoice=interest2;
 
-            }
+    }else if(interest2==="CSS"){
+      secondChoice=interest2;
+    }else{
+      secondChoice=interest2;
+
+    }
+  }
+
+}else {
+  alert("Please Enter A Name!!!!");
+}
+event.preventDefault();
+});
+});
