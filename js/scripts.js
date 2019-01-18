47f0415c13056a864ee6b880a027b933d43bc839
Jimmy Zebroski <zebrozkii@gmail.com>
2019-01-18 11:29:03 -0800

    working with if statements in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,18 @@
 $(document).ready(function(){
   $("#formLanguage").submit(function(event) {
-
-
-
 var theName = $("input#name").val();
 var theEnviroment = $("#enviroment").val();
+var interest1 =$("#interest1").val();
+var firstChoice;
 if(theName){
-  
+  if(theEnviroment==="Large Company or Business Software"||theEnviroment==="Enterprise-level Company or High Performance Applications"){
+    if(interest1==="C#"){
+      firstChoice=interest1;
+      }else{
+        firstChoice=interest1;
+      }
+    }else{
+    if (theEnviroment==="Fast Paced and possible government work"||theEnviroment==="Start ups and Smaller companies") {
+      }else {
 
-
-
-
-
-
-
-
-
-
-}else{
-alert("Please enter your name!!")
-}
-        event.preventDefault();
-  });
-});
+            }
