0eae88c4e136313ab77f1362f69a051ade1902e5
Jimmy Zebroski <zebrozkii@gmail.com>
2019-01-18 10:25:24 -0800

    set up text bar to recieve name and made a alert to make it so you have to enter a name, and started to work on javascript

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#formLanguage").submit(function(event) {
+
+
+
+var theName = $("input#name").val();
+var theEnviroment = $("#enviroment").val();
+if(theName){
+  
+
+
+
+
+
+
+
+
+
+
+}else{
+alert("Please enter your name!!")
+}
+        event.preventDefault();
+  });
+});
